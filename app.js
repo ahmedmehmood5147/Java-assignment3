bf04577824985f62fd3dd160bf2a3bb16d2381aa
ahmedmehmood5147 <am9474036@gmail.com>
2023-11-13 05:14:37 +0500

    Java-assignment3

---
+++ app.js
@@ -0,0 +1,10 @@
+var age = "I am 20 years old"
+alert (age)
+var visitors = "You have vistied this site 15 times"
+alert (visitors)
+var birthYear = "MY BIRTH YEAR IS 2003<br>"
+document.write (birthYear)
+var visitorName = "Ahmed"
+var quantity = "5"
+var title = "T-shirt"
+document.write (visitorName + " " + "ordered" + " " + quantity + " " + title + " " + "on Xyz clothing store")
